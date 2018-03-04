b1fbfdad45390163e9a6b0d3effbcc3a1a17f40e
Yuri Konotopov <ykonotopov@gnome.org>
2018-03-04 19:39:44 +0400

    mt66xx: cleanup

--- MediaTek/lib_driver_cmd_mt66xx/Android.mk
+++ MediaTek/lib_driver_cmd_mt66xx/Android.mk
@@ -27,17 +27,9 @@ WPA_SUPPL_DIR_INCLUDE += external/libnl/include
 WPA_SRC_FILE += mediatek_driver_cmd_nl80211.c
 endif
 
-ifdef CONFIG_DRIVER_WEXT
-#error doesn't support CONFIG_DRIVER_WEXT
-endif
-
 # To force sizeof(enum) = 4
 ifeq ($(TARGET_ARCH),arm)
 L_CFLAGS += -mabi=aapcs-linux
-endif
-
-ifdef CONFIG_ANDROID_LOG
-L_CFLAGS += -DCONFIG_ANDROID_LOG
 endif
 
 ########################
